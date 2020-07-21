770957857436221ce797a538d1c17e6e926dc7b8
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-21 13:16:12 -0400

    Endpoint 1 - Camelize

--- src/main/java/com/example/checkpoint/Controller.java
+++ src/main/java/com/example/checkpoint/Controller.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
 
 @RestController
 public class Controller {
@@ -17,8 +21,11 @@ public class Controller {
     }
 
     @GetMapping("/camelize")
-    public String camelize() {
-        return "Hello from Spring!";
+    public String getIndividualParams(
+            @RequestParam String original,
+            @RequestParam(required = false) boolean initialCap ) {
+            String camel = toCamelCase(original,initialCap,new char[]{'_'});
+        return String.format("%s", camel);
     }
 
     @GetMapping("/redact")
@@ -36,6 +43,55 @@ public class Controller {
         return "Hello from Spring!";
     }
 
+    public static String toCamelCase(String str, final boolean capitalizeFirstLetter, final char... delimiters) {
+        str = str.toLowerCase();
+        final int strLen = str.length();
+        final int[] newCodePoints = new int[strLen];
+        int outOffset = 0;
+        final Set<Integer> delimiterSet = generateDelimiterSet(delimiters);
+        boolean capitalizeNext = false;
+        if (capitalizeFirstLetter) {
+            capitalizeNext = true;
+        }
+        for (int index = 0; index < strLen;) {
+            final int codePoint = str.codePointAt(index);
+
+            if (delimiterSet.contains(codePoint)) {
+                capitalizeNext = true;
+                if (outOffset == 0) {
+                    capitalizeNext = false;
+                }
+                index += Character.charCount(codePoint);
+            } else if (capitalizeNext || outOffset == 0 && capitalizeFirstLetter) {
+                final int titleCaseCodePoint = Character.toTitleCase(codePoint);
+                newCodePoints[outOffset++] = titleCaseCodePoint;
+                index += Character.charCount(titleCaseCodePoint);
+                capitalizeNext = false;
+            } else {
+                newCodePoints[outOffset++] = codePoint;
+                index += Character.charCount(codePoint);
+            }
+        }
+        if (outOffset != 0) {
+            return new String(newCodePoints, 0, outOffset);
+        }
+        return str;}
+
+    private static Set<Integer> generateDelimiterSet(final char[] delimiters) {
+        final Set<Integer> delimiterHashSet = new HashSet<>();
+        delimiterHashSet.add(Character.codePointAt(new char[]{' '}, 0));
+        if (delimiters == null || delimiters.length == 0) {
+            return delimiterHashSet;
+        }
+
+        for (int index = 0; index < delimiters.length; index++) {
+            delimiterHashSet.add(Character.codePointAt(delimiters, index));
+        }
+        return delimiterHashSet;
+    }
 }
+
+
+
 
 
